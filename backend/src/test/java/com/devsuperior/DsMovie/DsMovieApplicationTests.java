e50bd639823d3ede3f2dfc910e7591fed5017c13
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-01-12 20:03:36 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/DsMovie/DsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DsMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
